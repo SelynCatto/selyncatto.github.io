d95ccb55ea52bfc1c991d2ee809ed54451b99520
ItsLynix <lynixthewolf@gmail.com>
2024-10-09 12:09:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Nice to see you here! This is the first website I have made in WebStorm')
+
+function takeMeHomeDaddy() {
+    history.go(-1)
+}
+
+function getGithub() {
+    window.location.href = "https://github.com/SelynCatto";
+}
+
+function getTelegram() {
+    window.location.href = "https://t.me/SelynCatto";
+}
